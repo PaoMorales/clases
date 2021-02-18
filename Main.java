4bd9baeae1d98e0fa3cd5f730f8c10a5728603ee
unknown <paola_morales12@outlook.com>
2021-02-18 01:15:30 -0600

    checando

---
+++ Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+class Persona {
+    private int edad;
+    private String name;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void cumplirAnios() {
+        edad++;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+
+        Persona panchito = new Persona();
+        Persona lupita = new Persona();
+
+        panchito.setName("Panchito");
+        lupita.setName("Lupita");
+
+        panchito.cumplirAnios();
+        panchito.cumplirAnios();
+        panchito.cumplirAnios();
+        panchito.cumplirAnios();
+
+        lupita.cumplirAnios();
+
+        System.out.println(panchito.getName() + " tiene ".toUpperCase() + panchito.getEdad() + "años");
+        System.out.println(lupita.getName() + " tiene " + lupita.getEdad() + " años");
+    }
+}
